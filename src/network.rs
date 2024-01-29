967f58c866660b8dd69160fda1081f9488f04bf3
Hiroki Oshima <hiroki.oshima@gmail.com>
2024-01-28 22:32:58 -0800

    fixed moved value error

--- src/network.rs
+++ src/network.rs
@@ -16,27 +16,28 @@ pub fn search_peers() -> std::io::Result<UdpSocket> {
     let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, port))
         // .into_iter()
         .map_err(with("failed to bind"))?;
-    let s = Ok(socket);
+    let s = &socket;
     socket
         .join_multicast_v4(&multicast, &Ipv4Addr::UNSPECIFIED)
         .map_err(with("failed to join multicast"))
-        .and(s)?
+        .and(Ok(s))?
         .set_multicast_loop_v4(false)
         .map_err(with("failed to disable multicast loop"))
-        .and(s)?
+        .and(Ok(s))?
         .set_write_timeout(timeout)
         .map_err(with("failed to set write timeout"))
-        .and(s)?
+        .and(Ok(s))?
         .set_read_timeout(timeout)
         .map_err(with("failed to set read timeout"))?;
     const MSG: &[u8; 9] = b"cat-choco";
     let peers = (0..1000)
         .map(|_| -> std::io::Result<_> {
-            let sent = s.send_to(MSG, (multicast, port))?;
+            let sent = socket.send_to(MSG, (multicast, port))?;
             assert!(sent == MSG.len());
             let mut buf = MSG.clone();
             buf.fill(0);
-            s.recv_from(&mut buf)
+            socket
+                .recv_from(&mut buf)
                 .ok()
                 .filter(|(recv, _)| *recv == MSG.len() && buf == *MSG)
                 .map(|(_, addr)| Ok(addr))
@@ -47,9 +48,10 @@ pub fn search_peers() -> std::io::Result<UdpSocket> {
     peers
         .first()
         .ok_or(Error::other("no peer found"))
-        .and_then(|p| s.connect(p))?;
-    s.leave_multicast_v4(&multicast, &Ipv4Addr::UNSPECIFIED)
+        .and_then(|p| socket.connect(p))?;
+    socket
+        .leave_multicast_v4(&multicast, &Ipv4Addr::UNSPECIFIED)
         .expect("failed to leave multicast");
 
-    Ok(s)
+    Ok(socket)
 }
